209d384aee1a3a157979f4b146965df26cd02d8f
Yakout97 <75516344+Yakout97@users.noreply.github.com>
2023-03-06 05:52:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// Selectors
+
+const inputBtn = document.querySelector('.input-btn');
+const itemWrap = document.querySelector('.item-wrap');
+const input = document.querySelector('.input');
+const checkListbtn = document.querySelector('.fa-check-square-o');
+const checkList = document.querySelector('.checked-list-position');
+
+
+// EventListener
+
+inputBtn.addEventListener("click", addItem);
+checkListbtn.addEventListener("click", addInList);
+// Functions
+
+function addItem (a) {
+    const value = input.value;
+//conditons for check if the input is empty doesn't excute the code
+    if (value === "") {
+        alert("Warning! the input is empty");
+    }
+    else
+    {
+//Creat li
+    const itemWrap = document.querySelector('.item-wrap');
+    const creatLi = document.createElement('li');
+    creatLi.classList.add('item');
+    itemWrap.appendChild(creatLi);
+//creat child
+    const creatChild = document.createElement('div');
+    const textChild = document.createTextNode(value);
+    creatChild.classList.add('item-child');
+    creatChild.appendChild(textChild);
+    creatLi.appendChild(creatChild);
+    input.value = ''; // for remove value ofter click btn from input
+// creat Check btn 
+    const creatBtn = document.createElement('button');
+    const btnCheck = document.createElement('i');
+    creatBtn.classList.add('btn-check');
+    btnCheck.classList.add('fa','fa-check');
+    creatBtn.appendChild(btnCheck);
+    creatLi.appendChild(creatBtn);
+// creat Trash btn
+    const creatBtn2 = document.createElement('button');
+    const btnTrash = document.createElement('i');
+    creatBtn2.classList.add('btn-delete');
+    btnTrash.classList.add('fa','fa-trash');
+    creatBtn2.appendChild(btnTrash);
+    creatLi.appendChild(creatBtn2);
+
+    // remove trash btn
+
+    creatBtn2.addEventListener('click',async function () {
+        creatLi.classList.add('remove-style');
+        await new Promise((resolve) => {
+            setTimeout(resolve, 660)
+        })
+        creatLi.remove();
+
+    })
+
+    // style check btn
+
+    creatBtn.addEventListener("click", function () {
+
+        if (creatChild.classList == "item-child", creatLi.classList == "item") {
+            creatChild.classList.add('cheked');
+            creatLi.classList.add('cheked-background');
+
+        } else {
+            
+            creatChild.classList.remove('cheked');
+            creatLi.classList.remove('cheked-background');
+        }
+    })
+
+    }
+}
+
+
